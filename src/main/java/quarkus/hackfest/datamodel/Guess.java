acce93e24607e7896a48dca70a8a58d75f490520
Guilherme Camposo <guilherme.camposo@me.com>
2020-09-16 12:31:49 -0300

    initial commit

---
+++ src/main/java/quarkus/hackfest/datamodel/Guess.java
@@ -0,0 +1,24 @@
+package quarkus.hackfest.datamodel;
+
+public class Guess {
+
+   private String Gp;
+
+   private String driver;
+
+    public String getGp() {
+        return Gp;
+    }
+
+    public void setGp(String gp) {
+        Gp = gp;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+}
